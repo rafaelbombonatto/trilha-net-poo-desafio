0cfa915a993d2a809d124eba36a5d9818d64a0d1
Rafael Bombonatto Martins Fernandes <rafael.bombonatto@gmail.com>
2022-10-13 10:35:03 -0300

    Implementação da solução proposta no desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizando Testes no Nokia
+Console.WriteLine("Testes no Nokia");
+Nokia nokia = new Nokia("19981818181","ZPTO 2000", "321as32a1s3a1s",256);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("DIO");
+
+// Realizando Testes no  IPHONE
+Console.WriteLine("Testes no IPhone");
+Iphone iphone = new Iphone("19981818181","ixy 15b", "121asa123121",1000);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO");
